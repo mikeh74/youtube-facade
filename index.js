978a72514d1bb8440d56cf9e6d6b7097dcdb96aa
Mike Horrocks <michael.horrocks@hotmail.co.uk>
2025-01-20 22:25:21 +0000

    Basic functionality added

--- index.js
+++ index.js
@@ -1,29 +1,55 @@
 
+// const modalContent = documment.getElementById('yf-modal-placeholder');
+// console.log(modalContent);
+
 const els = document.querySelectorAll('.youtube-facade');
 
 console.log(els);
 
 els.forEach(el => {
-
   el.addEventListener('click', (e) => {
+    console.log('clicked');
     e.preventDefault();
 
     // try to get the video id from the data-youtube-id attribute first
     const videoId = getYoutubeVideoId(el);
 
     // create an iframe element and replace the current element with it
-    const iframe = document.createElement('iframe');
-    iframe.setAttribute('src', `https://www.youtube.com/embed/${videoId}?autoplay=1`);
-    iframe.setAttribute('width', '560');
-    iframe.setAttribute('height', '315');
-    iframe.setAttribute('class', 'youtube-facade-iframe');
-    iframe.setAttribute('frameborder', '0');
-    iframe.setAttribute('allow', 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture');
-    iframe.setAttribute('allowfullscreen', '');
+    const iframe = createYouTubeIframe(videoId);
+    console.log(iframe);
 
-    el.replaceWith(iframe);
+    // check if the element has the data-youtube-modal attribute
+    const modal = el.getAttribute('data-youtube-modal');
+
+    if (modal) {
+      // toggle the modal
+      toggleModal();
+      const modalContent = document.querySelector('#youtube-facade-modal-placeholder');
+      modalContent.appendChild(iframe);
+    } else {
+      // replace the element with the iframe
+      el.replaceWith(iframe);
+    }
   });
 });
+
+
+const toggleModal = () => {
+  const modal = document.querySelector('.youtube-facade-modal');
+  modal.classList.toggle('youtube-facade-modal-active');
+}
+
+function createYouTubeIframe(videoId) {
+  const iframe = document.createElement('iframe');
+  iframe.setAttribute('src', `https://www.youtube.com/embed/${videoId}?autoplay=1`);
+  iframe.setAttribute('width', '560');
+  iframe.setAttribute('height', '315');
+  iframe.setAttribute('class', 'youtube-facade-iframe');
+  iframe.setAttribute('frameborder', '0');
+  iframe.setAttribute('allow', 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture');
+  iframe.setAttribute('allowfullscreen', '');
+  return iframe;
+}
 
 /**
  * Get the youtube video id from the element data-youtube-id attribute
@@ -41,7 +67,7 @@ function getYoutubeVideoId(el) {
   if (videoId) {
     console.log(videoId);
     return videoId;
-  } 
+  }
 
   // parse href of the element and get the video id from the v parameter
   const href = el.getAttribute('href');
@@ -51,3 +77,13 @@ function getYoutubeVideoId(el) {
   console.log(videoId);
   return videoId;
 }
+
+const closeButtons = document.querySelectorAll('.youtube-facade-modal-close');
+
+closeButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    toggleModal();
+    const modalContent = document.querySelector('#youtube-facade-modal-placeholder');
+    modalContent.innerHTML = '';
+  });
+});
