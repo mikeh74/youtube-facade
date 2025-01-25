aca481e7c3aefece6be906974031485bcc77b3dd
Mike Horrocks <michael.horrocks@hotmail.co.uk>
2025-01-25 22:21:13 +0000

    Updating SCSS

--- index.js
+++ index.js
@@ -208,7 +208,7 @@ document.addEventListener('DOMContentLoaded', () => {
       <div class="youtube-facade-modal-content">
         <button class="youtube-facade-modal-close" aria-label="Close modal">
           <svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-            <path fill="#ffffff" d="M 16.830797 20.000301 L 10.000151 13.16957 L 3.169428 20.000301 L -0 16.830883 L 6.830741 10.000151 L -0 3.169418 L 3.169428 0 L 10.000151 6.830732 L 16.830797 0 L 20 3.169418 L 13.16958 10.000151 L 20 16.830883 Z"/>
+            <path class="youtube-facade-model-close-svg" fill="#ffffff" d="M 16.830797 20.000301 L 10.000151 13.16957 L 3.169428 20.000301 L -0 16.830883 L 6.830741 10.000151 L -0 3.169418 L 3.169428 0 L 10.000151 6.830732 L 16.830797 0 L 20 3.169418 L 13.16958 10.000151 L 20 16.830883 Z"/>
           </svg>
         </button>
         <div id="youtube-facade-modal-placeholder" class="youtube-facade-modal-inner">
